7b1f40ecbae2725e9cbb59d85f0ab54d234bcb1d
DavidDevGt <josuedavidvl18@gmail.com>
2024-09-13 00:32:15 -0600

    Add initial server setup and configuration

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { connectDB } = require('./config/db');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Galileo News API v0.1');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
