59d420fb6e275941e789c7faacf283be4c1ba9d2
DavidDevGt <josuedavidvl18@gmail.com>
2024-09-16 01:24:01 -0600

    Update allowNull for LinkContacto and NoticiaEvento models

--- src/models/LinkContacto.js
+++ src/models/LinkContacto.js
@@ -23,7 +23,7 @@ LinkContacto.init({
     },
     noticia_evento_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
             model: 'NoticiaEventos',
             key: 'id'
--- src/models/NoticiaEvento.js
+++ src/models/NoticiaEvento.js
@@ -23,7 +23,7 @@ NoticiaEvento.init({
     },
     categoria_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
             model: 'Categoria',
             key: 'id'
