9446f63c1e8b60c8bbbf78ca1b3553684b05dbf7
daviddevgt <josuedavidvl18@gmail.com>
2025-01-06 20:27:45 -0600

    🐛 fix(bug): public routes access

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,18 +1,24 @@
 const AuthService = require("../services/AuthService");
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+  // Excluir rutas públicas
+  const publicRoutes = ["/auth/login", "/auth/register"];
+  if (publicRoutes.includes(req.path)) {
+    return next(); // No aplica autenticación en rutas públicas
+  }
 
+  // Validar token
+  const token = req.header("Authorization")?.replace("Bearer ", "");
   if (!token) {
     return res.status(401).json({ message: "No existe token" });
   }
 
   try {
     const decoded = AuthService.verifyToken(token);
-    req.user = decoded;
+    req.user = decoded; // Adjuntar información del usuario al request
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Token no valido" });
+    return res.status(401).json({ message: "Token no válido" });
   }
 };
 
